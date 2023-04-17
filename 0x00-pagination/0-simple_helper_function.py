10edcd1481f6b7b2487023744640193c02e32c4b
apha-pesso <mutiuayo99@gmail.com>
2023-04-17 12:40:27 +0100

    Create helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """Takes two integer arguments page and page_size
+    and return tuple"""
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page > 0
+
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
