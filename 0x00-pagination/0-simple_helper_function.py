f39b3d01a307ee36350e42c196879f2775116b2d
apha-pesso <mutiuayo99@gmail.com>
2023-04-17 12:42:32 +0100

    Raise Error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,8 +6,11 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple:
     """Takes two integer arguments page and page_size
     and return tuple"""
-    assert isinstance(page, int) and page > 0
-    assert isinstance(page_size, int) and page > 0
+    try:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page > 0
+    except AssertionError:
+        raise
 
     start_index = (page - 1) * page_size
     end_index = page * page_size
