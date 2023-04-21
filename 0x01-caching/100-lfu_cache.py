f797f766a8d55b88ff375659b3db81f0fffed59e
apha-pesso <mutiuayo99@gmail.com>
2023-04-21 17:59:04 +0100

    Fix replacement logic

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -17,26 +17,62 @@ class LFUCache(BaseCaching):
         """Assign item to key"""
         if len(self.cache_data) == BaseCaching.MAX_ITEMS:
             if key and item and key not in self.key_list:
+                # if key and item:
 
                 values = list(self.key_dict.values())
                 # min_value = min(list(self.key_dict.values()))
                 min_value = min(values)
 
-                count = values.count(min_value)
+                # count = values.count(min_value)
                 # for i in range(count):
-                for key in self.key_list:
+                # for k in self.key_list:
+                # a = 0
+                # print("Check")
+                # print(self.key_list)
+                # print(self.key_dict)
+                # print(min_value)
+                # print("Check")
+                for k in self.key_list:
                     for keye, value in self.key_dict.items():
-                        if value == min_value:
+                        if value == min_value and k == keye:
                             my_key = keye
+                            print(keye)
+                            break
+                    try:
+                        if my_key:
+                            break
+                    except UnboundLocalError:
+                        pass
+
+                """
+                for keye, value in self.key_dict.items():
+                    # print(keye, value, min_value)
+                    # print(self.key_list)
+                    # print(self.key_dict)
+                    if value == min_value and keye == self.key_list[a]:
+                        print(keye, value, value, self.key_list[a])
+                        my_key = keye
+                        print(a)
+                        break
+                    a += 1
+                """
 
                 try:
                     self.key_list.remove(my_key)
                 except ValueError:
                     pass
+
+                try:
+                    # self.cache_data.pop(my_key)
+                    self.key_dict.pop(my_key)
+                except KeyError:
+                    pass
+
                 try:
                     self.cache_data.pop(my_key)
                 except KeyError:
                     pass
+
                 print("DISCARD: {}".format(my_key))
 
         if key and item:
